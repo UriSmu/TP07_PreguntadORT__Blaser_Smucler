a1494de73ed09174e8c70955c9f4c92eee787ffb
URIEL MATIAS SMUCLER <48240113@ort>
2024-08-29 15:08:33 -0300

    Avances bloque 1 29/8

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+using System;
+namespace TP6__JJOO__Blaser_Smucler.Models;
+
+public class Categoria
+{
+    public int IdCategoria {get; set;}
+    public string Nombre {get; set;}
+    public string Foto {get; set;}
+
+    public Categoria() {}
+
+    public Categoria(int id, string nombre, string foto)
+    {
+        IdCategoria = id;
+        Nombre = nombre;
+        Foto = foto;
+    }
+}
