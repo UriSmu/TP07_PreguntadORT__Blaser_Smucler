7144d641dba532e609f3f92c314affa67dc45c80
NICOLAS GAEL BLASER <48801536@ort>
2024-08-29 15:26:52 -0300

    Avances

---
+++ Models/Respuesta.cs
@@ -0,0 +1,25 @@
+using System;
+namespace TP6__JJOO__Blaser_Smucler.Models;
+
+public class Respuesta
+{
+    public int IdRespuesta {get; set;}
+    public int IdPregunta {get; set;}
+    public int Opcion {get; set;}
+    public string Contenido {get; set;}
+    public bool Correcta {get; set;}
+    public string Foto {get; set;}
+
+
+    public Respuesta() {}
+
+    public Respuesta(int id, int Id, int opcion, string contenido, bool correcta, string foto)
+    {
+        IdRespuesta = id;
+        IdPregunta = Id;
+        Opcion = opcion;
+        Contenido = contenido;
+        Correcta = correcta;
+        Foto = foto;
+    }
+}
