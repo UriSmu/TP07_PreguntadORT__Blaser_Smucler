b01f1d46bd7c42bd46d56447adce4d8de3b79036
URIEL MATIAS SMUCLER <48240113@ort>
2024-09-03 07:54:14 -0300

    Inicio clase 3/9

--- Models/BD.cs
+++ Models/BD.cs
@@ -18,5 +18,43 @@ static public class BD
         return Lista;
     }
 
-    public static List<Dificultad>
+    public static List<Dificultad> ObtenerDificultades()
+    {
+        List<Dificultad> Lista = null;
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Dificultad>("SELECT * FROM Dificultades").ToList();
+        }       
+        return Lista;
+    }
+
+    public static List<Pregunta> ObtenerPreguntas(int dificultad, int categoria)
+    {
+        List<Pregunta> Lista = null;
+        string sql = " ";
+        if(dificultad != -1 && categoria != -1)
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdDificultad = @pDificultad and IdCategoria = @pCategoria";
+        }
+        else if(dificultad == -1 && categoria == -1)
+        {
+            sql = "SELECT * FROM Preguntas";
+        }
+        else if(categoria == -1)
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdDificultad = @pDificultad";
+        }
+        else
+        {
+            sql = "SELECT * FROM Preguntas WHERE IdCategoria = @pCategoria";
+        }
+
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            Lista = db.Query<Pregunta>(sql, new{pDificultad = dificultad, pCategoria = categoria}).ToList();
+        }       
+        return Lista;
+    }
+
+
 }
---
+++ Models/Juego.cs
@@ -0,0 +1,6 @@
+namespace TP6__JJOO__Blaser_Smucler.Models;
+
+static public class Juego
+{
+    
+}
